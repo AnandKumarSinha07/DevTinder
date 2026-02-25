0460078605e873944e7577d38a8366af4f26bcb3
Anand Kumar Sinha <109061880+AnandKumarSinha07@users.noreply.github.com>
2026-02-26 02:32:19 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -16,11 +16,19 @@ const requestRouter=require('../src/router/request');
 const chatRotuter=require('../src/router/chat')
 const initializeSocket = require("./utils/socket");
 
+
+// 1. Pehle yeh line add karo (Cookies ke liye zaroori hai)
+app.set("trust proxy", 1); 
 app.use(express.json());
 app.use(cookieParser());
 
-app.use(cors())
-
+// 2. CORS ko aise likho
+app.use(cors({
+  origin: "https://devtinderui-ga3q.onrender.com", // Sirf domain, no path!
+  credentials: true, 
+  methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"]
+}));
 app.use("/",authrouter);
 app.use('/',chatRotuter);
 app.use("/",ProfileRouter);
