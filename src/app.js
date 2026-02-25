c0ee8500d81c4695c11808377adf6a4f909f9787
AnandKumarSinha07 <anandkr7808@gmail.com>
2026-02-25 13:37:26 +0530

    new new cors changes

--- src/app.js
+++ src/app.js
@@ -19,8 +19,12 @@ const initializeSocket = require("./utils/socket");
 app.use(express.json());
 app.use(cookieParser());
 
-
-app.use(cors());
+app.use(cors({
+  origin: [
+    "http://localhost:5173",
+  ],
+  credentials: true
+}));
 
 
 app.use("/",authrouter);
