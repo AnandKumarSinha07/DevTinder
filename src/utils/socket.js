1b49d1ce446faaba3b0de4d20471edd6c57c17bd
Anand Kumar Sinha <109061880+AnandKumarSinha07@users.noreply.github.com>
2026-02-28 14:22:12 +0530

    Update socket.js

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -11,20 +11,18 @@ const initializeSocket = (server) => {
     });
 
     io.on("connection", (socket) => {
-        // User joins a private room
-        socket.on("joinChat", ({ firstName, userId, targetUserid }) => {
-            const room = [userId, targetUserid].sort().join("_");
-            console.log(firstName + " joined room: " + room);
-            socket.join(room);
+       
+        socket.on("joinChat", ({ userId, targetUserid }) => {
+            const roomId = [userId, targetUserid].sort().join("_");
+            socket.join(roomId);
+            console.log("User joined room: " + roomId);
         });
 
-        // Sending a message
+      
         socket.on("sendMessage", async ({ firstName, userId, targetUserid, text }) => {
             const roomId = [userId, targetUserid].sort().join("_");
-            console.log(firstName + " says: " + text);
 
             try {
-                // Find or Create Chat Document in Database
                 let newChat = await chat.findOne({
                     participants: { $all: [targetUserid, userId] }
                 });
@@ -36,25 +34,25 @@ const initializeSocket = (server) => {
                     });
                 }
 
-                // Push new message to history
-                newChat.message.push({
-                    senderId: userId,
-                    text
-                });
-
+                newChat.message.push({ senderId: userId, text });
                 await newChat.save();
-                socket.to(roomId).emit("messageReceived", { firstName, text });
+
+                
+                io.to(roomId).emit("messageReceived", { 
+                    firstName, 
+                    text, 
+                    senderId: userId 
+                });
 
             } catch (error) {
                 console.log("Error inside sendMessage:", error);
             }
         });
 
-        // Corrected spelling from 'disconnet' to 'disconnect'
         socket.on("disconnect", () => {
             console.log("User disconnected");
         });
     });
 };
 
-module.exports = initializeSocket;    
+module.exports = initializeSocket;
